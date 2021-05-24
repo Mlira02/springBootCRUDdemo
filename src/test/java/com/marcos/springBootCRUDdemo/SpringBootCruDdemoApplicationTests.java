4e2d9ea0315a59a98b374cff808a2e9acc70a310
Mlira02 <liram1995@gmail.com>
2021-05-24 14:23:40 -0500

    Initial setup and commit

---
+++ src/test/java/com/marcos/springBootCRUDdemo/SpringBootCruDdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.springBootCRUDdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCruDdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
